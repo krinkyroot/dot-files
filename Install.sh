bf65b09eed116424ceb84ccdbd1f0d3adcbd9837
krinkyroot <fzzy593@gmail.com>
2026-08-08 00:38:04 -0600

    Improve visual feedback in Install.sh
    Enhanced output formatting and color coding for user prompts and installation messages.

--- Install.sh
+++ Install.sh
@@ -30,55 +30,38 @@ cat << 'EOF'
 ⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
 EOF
 
-#!/bin/bash
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
 
-clear
+echo -e "${CYAN}"
+echo "████████████████████████████████████"
+echo "        K R I N K Y R O O T"
+echo "              DOTFILES"
+echo "████████████████████████████████████"
+echo -e "${RESET}"
 
-cat << 'EOF'
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠠⣾⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⠋⠁⠀⠀⠈⠹⣿⣿⣿⣿⣿⡿⠋⠀⠀⠈⠻⣿⣿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣿⣿⣿⣿⠃⠀⠀⠀⣴⣶⡄⢹⣿⣿⣿⣿⠃⢰⣶⡄⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣿⣿⣿⣿⡆⠀⠀⠀⠹⠿⠁⣸⣿⣿⣿⣿⡀⠘⡿⠃⠀⢀⣿⣿⣿⡆⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣯⣹⣿⣷⣤⣾⣿⣿⣿⣿⣿⣿⣃⣀⣀⣀⣀⠀
-⠀⠾⠿⠟⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠋⠉⠉⠋⠀
-⠠⣤⣤⣶⡶⠿⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠛⠛⠛⠷⣶⡄
-⠀⠀⠉⢀⣠⣶⠾⠟⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⠛⠳⢶⣦⣄⡀⠀⠀
-⠀⠀⠀⠟⠋⠁⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠁⠀⠀⠀⠀⠀⠀⠀⠉⠁⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
-EOF
-
-echo ""
-echo " K R I N K Y R O O T"
-echo " DOTFILES"
-echo ""
-echo "================================"
+echo -e "${BLUE}================================${RESET}"
 echo ""
 
-echo "[1] Install everything"
-echo "[2] Install Fastfetch"
-echo "[3] Install Fish"
-echo "[4] Install Kitty"
-echo "[5] Install Tofi"
-echo "[6] Install DerNoxium"
-echo "[7] Exit"
+echo -e "${GREEN}[1]${RESET} Install everything"
+echo -e "${CYAN}[2]${RESET} Install Fastfetch"
+echo -e "${YELLOW}[3]${RESET} Install Fish"
+echo -e "${MAGENTA}[4]${RESET} Install Kitty"
+echo -e "${BLUE}[5]${RESET} Install Tofi"
+echo -e "${CYAN}[6]${RESET} Install Dernoxium KDE Theme"
+echo -e "${GREEN}[7]${RESET} Install Nerd Font"
+echo -e "${RED}[8]${RESET} Exit"
 
 echo ""
-read -p "Choose an option: " choice
+echo -ne "${WHITE}Choose an option: ${RESET}"
+read choice
 
 mkdir -p ~/.config
 mkdir -p ~/.local/share/plasma/look-and-feel
@@ -86,51 +69,74 @@ mkdir -p ~/.local/share/plasma/look-and-feel
 case $choice in
 
 1)
-    cp -r fastfetch ~/.config/
-    cp -r fish ~/.config/
-    cp -r kitty ~/.config/
-    cp -r tofi ~/.config/
-    cp -r dernoxium ~/.local/share/plasma/look-and-feel/
+    echo -e "${CYAN}Installing everything...${RESET}"
 
-    echo "Everything installed!"
+    cp -r fastfetch ~/.config/
+    echo -e "${GREEN}✓ Fastfetch installed${RESET}"
+
+    cp -r fish ~/.config/
+    echo -e "${GREEN}✓ Fish installed${RESET}"
+
+    cp -r kitty ~/.config/
+    echo -e "${GREEN}✓ Kitty installed${RESET}"
+
+    cp -r tofi ~/.config/
+    echo -e "${GREEN}✓ Tofi installed${RESET}"
+
+    cp -r dernoxium ~/.local/share/plasma/look-and-feel/
+    echo -e "${GREEN}✓ Dernoxium KDE theme installed${RESET}"
+
+    echo -e "${CYAN}Installing JetBrainsMono Nerd Font...${RESET}"
+    sudo pacman -S --needed ttf-jetbrains-mono-nerd
+
+    echo -e "${GREEN}✓ Nerd Font installed${RESET}"
+    echo ""
+    echo -e "${GREEN}Everything installed successfully!${RESET}"
     ;;
 
 2)
     cp -r fastfetch ~/.config/
-    echo "Fastfetch installed!"
+    echo -e "${GREEN}✓ Fastfetch installed!${RESET}"
     ;;
 
 3)
     cp -r fish ~/.config/
-    echo "Fish installed!"
+    echo -e "${GREEN}✓ Fish installed!${RESET}"
     ;;
 
 4)
     cp -r kitty ~/.config/
-    echo "Kitty installed!"
+    echo -e "${GREEN}✓ Kitty installed!${RESET}"
     ;;
 
 5)
     cp -r tofi ~/.config/
-    echo "Tofi installed!"
+    echo -e "${GREEN}✓ Tofi installed!${RESET}"
     ;;
 
 6)
     cp -r dernoxium ~/.local/share/plasma/look-and-feel/
-    echo "DerNoxium installed!"
+    echo -e "${GREEN}✓ Dernoxium KDE theme installed!${RESET}"
     ;;
 
 7)
-    echo "Exiting..."
+    echo -e "${CYAN}Installing JetBrainsMono Nerd Font...${RESET}"
+    sudo pacman -S --needed ttf-jetbrains-mono-nerd
+    echo -e "${GREEN}✓ Nerd Font installed!${RESET}"
+    ;;
+
+8)
+    echo -e "${RED}Exiting...${RESET}"
     exit 0
     ;;
 
 *)
-    echo "Invalid option!"
+    echo -e "${RED}✗ Invalid option!${RESET}"
     exit 1
     ;;
 
 esac
 
 echo ""
-echo "Done!"
+echo -e "${CYAN}Done!${RESET}"
+```
