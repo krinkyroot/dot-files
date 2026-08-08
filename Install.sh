70412c93231e2ea8e0189b1d0565d810d26d8e04
krinkyroot <fzzy593@gmail.com>
2026-08-07 20:33:33 -0600

    Added epic installer
    epic

---
+++ Install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing super kewl dotfiles"
+
+mkdir -p ~/.config
+
+cp -r fastfetch ~/.config/
+cp -r fish ~/.config/
+cp -r kitty ~/.config/
+cp -r tofi ~/.config/
+
+echo "haxor files installed -mr robot real"
